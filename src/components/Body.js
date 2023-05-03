7d6f39136cbaad83f09575c65259952fe9aa6930
FirozSkAhmad <you@example.com>
2023-05-03 20:20:05 +0530

    understanding of react fiber, diff algo, virtual dom

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,21 @@
 import ResCard from './ResCard'
 import resList from '../../utils/mockData'
+import { useState } from 'react'
 
 const Body = () => {
+
+    const [data, setData] = useState(resList)
+
+    function clickHandler() {
+        console.log("button clicked")
+        const filteredData = data.filter(x => x.data.avgRating > 4).sort((a, b) => b.data.avgRating - a.data.avgRating)
+        return setData(filteredData)
+    }
     return (
         <div className="body">
-            <h2>Search</h2>
+            <button className='filter-btn' onClick={clickHandler}>Top Rated Resturants</button>
             <div className="res-con">
-                {resList.map(singleResData => <ResCard key={singleResData.data.id} resData={singleResData} />)}
+                {data.map(singleResData => <ResCard key={singleResData.data.id} resData={singleResData} />)}
             </div>
         </div>
 
